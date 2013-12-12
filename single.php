e1cf4c408d1c8276ce051cdd962b99a0b591a6cf
Michael Shuh <michael.shuh@gmail.com>
2013-12-11 20:46:29 -0600

    modifying single post view

--- single.php
+++ single.php
@@ -9,23 +9,28 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+            <div class="container">
+                <div class="row-fluid">
+                    <div class="span8">
 
-		<?php while ( have_posts() ) : the_post(); ?>
+                    <?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'content', 'single' ); ?>
+                        <?php get_template_part( 'content', 'single' ); ?>
 
-			<?php gracepointresources_post_nav(); ?>
+                        <?php
+                            // If comments are open or we have at least one comment, load up the comment template
+                            if ( comments_open() || '0' != get_comments_number() ) :
+                                comments_template();
+                            endif;
+                        ?>
 
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template
-				if ( comments_open() || '0' != get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
+                    <?php endwhile; // end of the loop. ?>
 
-		<?php endwhile; // end of the loop. ?>
+                    </div>
 
-		</main><!-- #main -->
+                </main><!-- #main -->
+            </div>
+        </div>
 	</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
