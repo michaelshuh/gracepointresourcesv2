0cf5bd8baf4866b668a648160f1e04251ad9ab44
Michael Shuh <michael.shuh@gmail.com>
2013-12-12 23:09:22 -0600

    styling well boxes

--- js/site.js
+++ js/site.js
@@ -1,3 +1,9 @@
 jQuery(function(){
     jQuery('#nav-single a').addClass('btn btn-info');
+    boxes = jQuery('.about.well');
+    maxHeight = Math.max.apply(
+      Math, boxes.map(function() {
+            return jQuery(this).height();
+      }).get());
+    boxes.height(maxHeight);
 });
