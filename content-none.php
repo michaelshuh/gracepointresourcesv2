38ae06711c248286ec98f0eb4a26eb00a9ac4144
James Lee <junghlee90@gmail.com>
2013-12-12 23:20:26 -0600

    container

--- content-none.php
+++ content-none.php
@@ -8,7 +8,7 @@
  */
 ?>
 
-<section class="no-results not-found">
+<section class="no-results not-found container">
 	<header class="page-header">
 		<h1 class="page-title"><?php _e( 'Nothing Found', 'gracepointresources' ); ?></h1>
 	</header><!-- .page-header -->
