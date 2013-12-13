30a13746486549ca5131d1aa11a99c9a5952020f
James Lee <junghlee90@gmail.com>
2013-12-12 22:32:32 -0600

    fixed search results page

--- content.php
+++ content.php
@@ -12,17 +12,4 @@
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
-
-	<footer class="entry-meta">
-		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-            <div class="entry-meta breadcrumb">
-                <?php gracepointresources_posted_on(); ?>
-                <nav id="nav-single" class="navigation post-navigation" role="navigation">
-                    <a class="btn btn-info pull-right btn-more" href="<?php the_permalink();?>">read more <i class="icon icon-chevron-right icon-white"></i></a>
-                </nav>
-            </div><!-- .entry-meta -->
-		<?php endif; // End if 'post' == get_post_type() ?>
-
-		<?php edit_post_link( __( 'Edit', 'gracepointresources' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-meta -->
 </article><!-- #post-## -->
--- page.php
+++ page.php
@@ -11,7 +11,7 @@
  */
 
 get_header(); ?>
-
+    <?php echo "I am in page.php!" ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
