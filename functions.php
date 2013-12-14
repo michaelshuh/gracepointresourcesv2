94aed3f7fa1152c476c1456266bf4800a544e329
Michael Shuh <michael.shuh@gmail.com>
2013-12-14 15:54:22 -0600

    styling the widgets

--- functions.php
+++ functions.php
@@ -72,10 +72,10 @@ function gracepointresources_widgets_init() {
 	register_sidebar( array(
 		'name'          => __( 'Category Sidebar', 'gracepointresources' ),
 		'id'            => 'category-sidebar',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'before_widget' => '<aside id="%1$s" class="widget box %2$s">',
 		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
 	) );
 }
 add_action( 'widgets_init', 'gracepointresources_widgets_init' );
