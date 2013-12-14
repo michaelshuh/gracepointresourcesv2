77fdd33271bb181f96a46502044dbd2a2661b9b2
Michael Shuh <michael.shuh@gmail.com>
2013-12-14 16:51:57 -0600

    adding an attachment plugin for sidebar requires that you add the plugin

--- sidebar.php
+++ sidebar.php
@@ -6,8 +6,17 @@
  */
 ?>
 	<div id="secondary" class="widget-area" role="complementary">
-		<?php do_action( 'before_sidebar' ); ?>
-		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
-
-		<?php endif; // end sidebar widget area ?>
+        <aside id="attachments" class="widget box">
+            <?php $attachments = new Attachments( 'attachments' ); /* pass the instance name */ ?>
+            <?php if( $attachments->exist() ) : ?>
+                <h3>Attachments</h3>
+                <ul>
+                    <?php while( $attachments->get() ) : ?>
+                        <li>
+                            <a href="<?php $attachments->url();?>" target="_blank"><?php echo $attachments->image('thumbnail');?><span class="offset1"><?php echo $attachments->field('title')?><span></a>
+                        </li>
+                    <?php endwhile; ?>
+                </ul>
+            <?php endif; ?>
+        </aside>
 	</div><!-- #secondary -->
