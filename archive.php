d621e9ac4c3e6f1ea632c90243d7b54a527b99e6
Michael Shuh <michael.shuh@gmail.com>
2013-12-14 18:19:16 -0600

    more modification to archive pages

--- archive.php
+++ archive.php
@@ -74,19 +74,21 @@ get_header(); ?>
 			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+            <div class="well span8">
+                <?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
+                    <?php
+                        /* Include the Post-Format-specific template for the content.
+                        * If you want to override this in a child theme, then include a file
+                        * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                        */
+                        get_template_part( 'content', get_post_format() );
+                    ?>
 
-			<?php endwhile; ?>
+                <?php endwhile; ?>
 
-			<?php gracepointresources_paging_nav(); ?>
+                <?php gracepointresources_paging_nav(); ?>
+            </div>
 
 		<?php else : ?>
 
--- content-none.php
+++ content-none.php
@@ -13,7 +13,7 @@
 		<h1 class="page-title"><?php _e( 'Nothing Found', 'gracepointresources' ); ?></h1>
 	</header><!-- .page-header -->
 
-	<div class="page-content">
+	<div class="page-content container">
 		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
 			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'gracepointresources' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
