1c8977721b4e2853c5e498b8f32f260d11222b21
James Lee <junghlee90@gmail.com>
2013-12-17 23:26:28 -0600

    styling for the navigation

--- functions.php
+++ functions.php
@@ -101,16 +101,11 @@ function gracepointresources_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'gracepointresources_scripts' );
 
-function register_my_menus() {
-	register_nav_menus(
-		array(
-			'header-menu' => __( 'Header Menu' ),
-			'extra-menu' => __( 'Extra Menu' )
-		)
-	);
+function register_my_menu() {
+	register_nav_menu('header-menu',__( 'Header Menu' ));
 }
 
-add_action( 'init', 'register_my_menus' );
+add_action( 'init', 'register_my_menu' );
 
 /**
  * Implement the Custom Header feature.
