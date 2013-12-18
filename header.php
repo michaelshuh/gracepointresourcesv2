896bb236a00683da016e2c55841017254a372716
James Lee <junghlee90@gmail.com>
2013-12-17 23:39:34 -0600

    working navigation

--- header.php
+++ header.php
@@ -50,7 +50,8 @@
                 </div>
             </div>
         <?php endif; ?>
-        <?php wp_nav_menu( array( 'theme_location' => 'header-  menu', 'container_class' => 'main-navigation' ) ); ?>
+        <?php wp_nav_menu( array( 'theme_location' => 'primary',
+            'container_class' => 'main-navigation' ) ); ?>
     </div><!-- /.navbar-wrapper -->
 
 	<div id="content" class="site-content">
