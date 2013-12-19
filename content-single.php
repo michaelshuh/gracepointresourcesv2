19eb467de8ee5786654ce180e26a1bf91d66d0dd
Michael Shuh <michael.shuh@gmail.com>
2013-12-18 21:56:39 -0600

    moving download attachments to top of the post

--- content-single.php
+++ content-single.php
@@ -9,8 +9,41 @@
             <?php gracepointresources_posted_on(); ?>
             <?php gracepointresources_post_nav(); ?>
         </div><!-- .entry-meta -->
-        <?php if(function_exists('pf_show_link')){echo pf_show_link();} ?>
-        <?php echo cpa_the_author(); ?>
+        <div class="entry-meta breadcrumb well">
+            <?php if(function_exists('pf_show_link')){echo pf_show_link();} ?>
+            <?php
+            $attachments = new Attachments( 'attachments' );
+            if ($attachments->exist() ) {
+                $files_to_zip = array();
+                while( $attachments->get() ) {
+                    array_push($files_to_zip, $attachments->url());
+                }
+
+                $zip = new ZipArchive();
+
+                # create a temp file & open it
+                $uploads = wp_upload_dir();
+                $tmp_location = $uploads['path'];
+                $FileName = sanitize_title(get_the_title($post_id)).".zip";
+                $zipFileName = $tmp_location.'/'.$FileName;
+                $zip->open($zipFileName, ZipArchive::CREATE);
+
+                # loop through each file
+                foreach($files_to_zip as $file){
+                    # download file
+                    $download_file = file_get_contents($file);
+                    #add it to the zip
+                    $zip->addFromString(basename($file),$download_file);
+                }
+
+                # close zip
+                $zip->close();
+            ?>
+            <a class="download-files" href="<?php echo get_template_directory_uri()."/inc/download.php?File=".$FileName;?>">Download All Attachments</a>
+            <?php } ?>
+            <?php echo cpa_the_author(); ?>
+        </div>
+
         <h1 class="entry-title"><?php the_title(); ?></h1>
     </header><!-- .entry-header -->
 
--- single.php
+++ single.php
@@ -41,34 +41,6 @@ get_header(); ?>
                                     </li>
                                     <?php endwhile; ?>
                                 </ul>
-                            <?php
-                                //$attachments = new Attachments( 'attachments' );
-                                $files_to_zip = array();
-                                while( $attachments->get() ) {
-                                    array_push($files_to_zip, $attachments->url());
-                                }
-
-                                $zip = new ZipArchive();
-
-                                # create a temp file & open it
-                                $uploads = wp_upload_dir(); 
-                                $tmp_location = $uploads['path'];
-                                $FileName = sanitize_title(get_the_title($post_id)).".zip";
-                                $zipFileName = $tmp_location.'/'.$FileName;        
-                                $zip->open($zipFileName, ZipArchive::CREATE);
-
-                                # loop through each file
-                                foreach($files_to_zip as $file){
-                                    # download file
-                                    $download_file = file_get_contents($file);
-                                    #add it to the zip
-                                    $zip->addFromString(basename($file),$download_file);
-                                }
-
-                                # close zip
-                                $zip->close();
-                            ?>
-                            <p><a href="<?php echo get_template_directory_uri()."/inc/download.php?File=".$FileName;?>">Download Files as Zip</a></p>
                             </aside>
                         <?php endif; ?>
                         <?php get_sidebar(); ?>
