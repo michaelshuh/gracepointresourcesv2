b5de45541710156e7b689aeb577fecfe887b9c22
James Lee <jung.h.lee@oracle.com>
2013-12-23 18:11:21 -0600

    category with search results

--- content-search_result.php
+++ content-search_result.php
@@ -7,6 +7,9 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+		<div>
+			<?php signle_cat_title(); ?>
+		</div>
 	</header><!-- .entry-header -->
 
 	<div class="entry-summary">
