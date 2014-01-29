084a207b543f64140886addd64c42f215541ef34
Michael Shuh <michael.shuh@gmail.com>
2014-01-28 23:56:18 -0600

    modifying page layout

--- content-page.php
+++ content-page.php
@@ -7,18 +7,20 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'gracepointresources' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-	<?php edit_post_link( __( 'Edit', 'gracepointresources' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
+    <div class="container">
+        <header class="entry-header">
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+        </header><!-- .entry-header -->
+    
+        <div class="entry-content">
+            <?php the_content(); ?>
+            <?php
+                wp_link_pages( array(
+                    'before' => '<div class="page-links">' . __( 'Pages:', 'gracepointresources' ),
+                    'after'  => '</div>',
+                ) );
+            ?>
+        </div><!-- .entry-content -->
+        <?php edit_post_link( __( 'Edit', 'gracepointresources' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
+    </div>
 </article><!-- #post-## -->
